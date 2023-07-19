3764edb17978a1cf7c829f9e920342f95f00ec5f
mobarakamir17 <mobarakamir17@gmail.com>
2023-07-19 18:37:21 -0400

    updates to checkboxes

--- productivity-tracker/src/components/ToDoList.js
+++ productivity-tracker/src/components/ToDoList.js
@@ -16,11 +16,13 @@ function ToDoList({ tasks, onAddToDo, completedItems, setCompletedItems }) {
   };
 
   function handleButton() {
-    // Add the new to-do item to the toDoList state
-    onAddToDo(toDo);
+    // Add the new to-do item to the toDoList state if it's not empty
+    if (toDo.trim() !== '') {
+      onAddToDo(toDo);
 
-    // Clear the input field after adding the to-do item
-    setToDo('');
+      // Clear the input field after adding the to-do item
+      setToDo('');
+    }
   }
 
   function handleCheckboxChange(index) {
@@ -34,17 +36,25 @@ function ToDoList({ tasks, onAddToDo, completedItems, setCompletedItems }) {
     <div id="toDoList">
       <h2>To Do List</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text" name="todo" />
+        <input type="text" name="todo" value={toDo} onChange={(e) => setToDo(e.target.value)} />
         <button type="submit">Add</button>
       </form>
-      <ul>
-        {tasks?.map((task, index) => (
-          <li key={index}>
-            <input type="checkbox" checked={task.checked} onChange={() => handleCheckboxChange(index)} />
-            <span>{task.text}</span>
-          </li>
-        ))}
-      </ul>
+      {tasks.length > 0 ? ( // Only render the to-do items if there are tasks in the list
+        <ul>
+          {tasks?.map((task, index) => (
+            <li key={index}>
+              <input
+                type="checkbox"
+                checked={task.checked}
+                onChange={() => handleCheckboxChange(index)}
+              />
+              <span>{task.text}</span>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No tasks to display.</p> // Render this message if there are no tasks in the list
+      )}
     </div>
   );
 }
